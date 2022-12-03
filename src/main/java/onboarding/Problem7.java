80d9695ec3dbaa677ee1c8639e3df5c4c09ad489
95grit <95grit@gmail.com>
2022-12-03 17:08:07 +0900

    Problem7 작성

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,77 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+    private static final int DEFAULT_SCORE = 0, VISITOR_SCORE = 1, FRIENDS_SCORE = 10;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+        Map<String, Integer> users = new HashMap<>();
+        calculate(visitors, users, getVisitorsCalculator());
+        Calculator friendCalculator = getFriendsCalculator();
+        for (List<String> pairedFriends : friends) {
+            calculate(pairedFriends, users, friendCalculator);
+        }
+        List<String> alreadyFriends = findByAlreadyFriends(user, friends);
+        removeByAlreadyFriends(users, alreadyFriends);
+        return toArrayListWithSort(users);
+    }
+
+    public static void calculate(List<String> src, Map<String, Integer> dest, Calculator calculator) {
+        for (String user : src) {
+            Integer score = dest.get(user);
+            if (score == null) {
+                score = DEFAULT_SCORE;
+            }
+            score += calculator.calculate();
+            dest.put(user, score);
+        }
+    }
+
+    public interface Calculator {
+        int calculate();
+    }
+
+    public static Calculator getVisitorsCalculator() {
+        return () -> VISITOR_SCORE;
+    }
+
+    public static Calculator getFriendsCalculator() {
+        return () -> FRIENDS_SCORE;
+    }
+
+    public static void removeByAlreadyFriends(Map<String, Integer> users, List<String> friends) {
+        for (String friend : friends) {
+            users.remove(friend);
+        }
+    }
+
+    public static List<String> findByAlreadyFriends(String target, List<List<String>> friends) {
+        Set<String> alreadyFriends = new HashSet<>();
+        alreadyFriends.add(target);
+        for (List<String> users : friends) {
+            if (users.contains(target)) {
+                String user = users.get(0);
+                alreadyFriends.add(user);
+            }
+        }
+        return new ArrayList<>(alreadyFriends);
+    }
+
+    public static ArrayList<String> toArrayListWithSort(Map<String, Integer> map) {
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(map.entrySet());
+        entries.sort(getSortComparator());
+        ArrayList<String> sorted = new ArrayList<>();
+        entries.forEach(user -> sorted.add(user.getKey()));
+        return sorted;
+    }
+
+    public static Comparator<Map.Entry<String, Integer>> getSortComparator() {
+        return (left, right) -> {
+            if (left.getValue().intValue() == right.getValue().intValue()) {
+                return left.getKey().compareTo(right.getKey());
+            }
+            return left.getValue().compareTo((right.getValue()));
+        };
     }
 }
